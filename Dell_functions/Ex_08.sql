4fad0977106ae139bbcce3d20fd41c31eb0a4487
Sergio Sabala Guerra <73126873+SabalaDH@users.noreply.github.com>
2021-06-07 17:11:13 +0200

    Explanation Ex_08.sql

--- Dell_functions/Ex_08.sql
+++ Dell_functions/Ex_08.sql
@@ -23,3 +23,7 @@ RETURNS numeric AS $$
 select SUM(price)/COUNT(price) from products;
 $$ LANGUAGE SQL;
 
+
+
+
+-- In these functions, we carry out the average price of the products table, one function in sql language and the other in plpgsql language. Finally we carry out another function without using avg (making the sum of all the prices and dividing this sum by the number of all the prices).
