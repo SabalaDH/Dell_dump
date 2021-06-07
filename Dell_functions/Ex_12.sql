948ff385b89df4f1eb6ba05218963bf3fed973e2
Sergio Sabala Guerra <73126873+SabalaDH@users.noreply.github.com>
2021-06-07 17:22:40 +0200

    Explanation Ex_12.sql

--- Dell_functions/Ex_12.sql
+++ Dell_functions/Ex_12.sql
@@ -5,3 +5,6 @@ RETURN $1;
 END;
 $$ LANGUAGE PLPGSQL;
 
+
+
+-- In this exercise we create a function with cursors, to encapsulate the query and thus be able to return several lines at the same time.
