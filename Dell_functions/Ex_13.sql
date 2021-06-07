0d0088cb1d325c61db0b5ccb1d3ba8ee33c846d8
Sergio Sabala Guerra <73126873+SabalaDH@users.noreply.github.com>
2021-06-07 17:24:51 +0200

    Explanation Ex_13.sql

--- Dell_functions/Ex_13.sql
+++ Dell_functions/Ex_13.sql
@@ -12,3 +12,6 @@ CREATE OR REPLACE FUNCTION mySum (text, text) RETURNS text AS $$
  SELECT $1 || $2
  $$ LANGUAGE SQL;
 
+
+
+-- We define several functions to perform sums, one with integer values, another with a decimal value and another with a string value.
